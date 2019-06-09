569862d09ebce947c7592f9d43a777b93015f76a
Pat <patriciomugarte@gmail.com>
2019-06-08 21:25:32 -0300

    Agregado de comentarios para separar el código entre pruebas y resolución.

--- Clase1_Node_Ejercicio1.js
+++ Clase1_Node_Ejercicio1.js
@@ -1,33 +1,31 @@
 // 1. Escriba una expresión utilizando métodos de array de ES6 (por ejemplo, filter y reduce) para calcular el valor total de las máquinas en la lista de inventario.
 
 const inventario = [
-  {tipo: "maquina", valor: 5000},
-  {tipo: "maquina", valor: 650},
-  {tipo: "silla", valor: 10},
-  {tipo: "sillon", valor: 1200},
-  {tipo: "maquina", valor: 77}
+  { tipo: "maquina", valor: 5000 },
+  { tipo: "maquina", valor: 650 },
+  { tipo: "silla", valor: 10 },
+  { tipo: "sillon", valor: 1200 },
+  { tipo: "maquina", valor: 77 }
 ]
-/*
+/*  //PRUEBAS CON EL filter
 let arrayMaquinas = inventario.filter(inventariado => inventariado.tipo == "maquina")
 console.log(arrayMaquinas)
-
 let arraySillas = inventario.filter(inventariado => inventariado.tipo == "silla")
 console.log()
 console.log(arraySillas)
-
 let arraySillon = inventario.filter(inventariado => inventariado.tipo == "sillon")
 console.log()
 console.log(arraySillon)
 */
 
-//ESTO YA FUNCIONA
+  //ESTO YA FUNCIONA
 let valorTotalMaquina = inventario.filter(maquinas => maquinas.tipo == "maquina")
-                                  .reduce(function(a,b){
-                                        a.valor += b.valor;
-                                        return a;
-                                        })
+  .reduce(function(a, b) {
+    a.valor += b.valor;
+    return a;
+  })
 
-/*
+/*  //PRUEBA PARA NO TENER QUE ESCRIBIR EL function
 let valorTotalMaquina = (inventario.filter(maquinas => maquinas.tipo == "maquina")).reduce(function(a,b){
   a.valor += b.valor
 } )
